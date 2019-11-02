c38c9e815c2ff00020c76602caccbe089ab6b6c9
YuanmingLeee <YLI056@e.ntu.edu.sg>
2019-11-02 18:50:22 +0800

    [web-app] finish

--- server/api/review_classfication.py
+++ server/api/review_classfication.py
@@ -1,3 +1,5 @@
+from fastapi import Form
+
 import json
 
 import requests_async as requests
@@ -15,7 +17,7 @@ class ReviewPredictionController(Controller):
         self._host_url = 'http://{}:{}'.format(mac_configs['host'], mac_configs['port'])
 
     @mapping('predict', methods=['POST'])
-    async def classify(self, *, sentence: str):
+    async def classify(self, *, sentence: str = Form(...)):
         params = {'sentence': sentence}
 
         response = await requests.post(self._host_url + '/predict', params=params)
@@ -26,7 +28,7 @@ class ReviewPredictionController(Controller):
         message = ''
         if response.ok:
             data = {
-                'review': response_dict['stars'],
+                'stars': response_dict['stars'],
             }
         else:
             message = response_dict['message']
--- web-app/src/page/home/container/HighlightTextContainer.ts
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import { AppState } from '../../../store';
-import HighlightText from '../component/HighlightText';
-
-const mapStateToProps = (state: AppState) => ({
-  text: state.text.ner.data,
-});
-
-export default connect(mapStateToProps)(HighlightText);
