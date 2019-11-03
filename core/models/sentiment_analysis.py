f24a33c9f196598ec40f12c056c343b736238a72
HAN SIMENG <hans0035@e.ntu.edu.sg>
2019-11-03 22:45:30 +0800

    update

---
+++ core/models/sentiment_analysis.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
+import pandas as pd
+
+#import a LEXICON
+
+
+def get_sentiment(text):
+	
+	sid = SentimentIntensityAnalyzer()
+
+	sentiment_score = sid.polarity_scores(text)
+
+	return sentiment_score
+
+df = pd.read_csv("data.csv")
+
+right = 0
+total = len(df)
+for index, row in df.iterrows():
+    sentiment_score = get_sentiment(row["text"])
+    print(sentiment_score)
+    if sentiment_score['compound'] >  0.05:
+    	if row['stars'] > 2.0:
+    		right += 1
+    elif sentiment_score['compound'] > -0.05:
+    	if row['stars'] == 2.0:
+    		right += 1
+    elif sentiment_score['compound'] < -0.05:
+    	if row['stars'] < 2.0:
+    		right += 1
+
+print(right / total)
